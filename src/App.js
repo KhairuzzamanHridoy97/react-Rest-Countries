0fa6786bb4ea9cc530c4d84d598b36136e42850d
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-02-27 14:29:23 +0600

    another commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './Component/Countries/Countries';
+import Person from './Component/Person/Person';
 
 function App() {
   return (
     <div className="App">
       <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
 
-function Countries (){
+// function Countries (){
  
   // data load kore ey state e rakhbo :: 
-  const [countries,setCoutries] = useState([]); // state decalre
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    .then(data=>setCoutries(data))
-  },[])
-  return(
-    <div>
-      <h2>Travel Around The World: </h2>
-      <h4>Availabel Countries: {countries.length}</h4>
-      {
-        countries.map(
-          country=> <Country
-          // name={country.name}
-          // capital={country.capital}
-          country={country}
-          >
-          </Country>
-        )
-      }
-    </div>
-  )
-};
+//   const [countries,setCoutries] = useState([]); // state decalre
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res=>res.json())
+//     .then(data=>setCoutries(data))
+//   },[])
+//   return(
+//     <div>
+//       <h2>Travel Around The World: </h2>
+//       <h4>Availabel Countries: {countries.length}</h4>
+//       {
+//         countries.map(
+//           country=> <Country
+//           // name={country.name}
+//           // capital={country.capital}
+//           country={country}
+//           >
+//           </Country>
+//         )
+//       }
+//     </div>
+//   )
+// };
 
-function Country (props){
-  const {name,capital} = props.country
-  return(
-    <div>
-        {/* <h2>Name: {name[0]}</h2> */}
-        <h2>Capital: {capital}</h2>
-    </div>
-  )
-}
+
 
 
 export default App;
